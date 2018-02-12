e82292a072a373a7128c377469f329a6c772602c
fusilli <piotrek@bardzofajny.net>
2018-02-12 11:03:18 +0100

    one writeFileSync call

--- create-env.js
+++ create-env.js
@@ -1,3 +1,2 @@
-const fs = require('fs')
-fs.writeFileSync('./.env', `CONTENTFUL_ACCESS_TOKEN=${process.env.CONTENTFUL_ACCESS_TOKEN}\n`)
-fs.writeFileSync('./.env', `CONTENTFUL_SPACE_ID=${process.env.CONTENTFUL_SPACE_ID}\n`)
+const fs = require('fs');
+fs.writeFileSync('./.env', `CONTENTFUL_ACCESS_TOKEN=${process.env.CONTENTFUL_ACCESS_TOKEN}\nCONTENTFUL_SPACE_ID=${process.env.CONTENTFUL_SPACE_ID}\n`);
