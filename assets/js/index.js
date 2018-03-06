c1dd8fda5e896ae297a518dd4b0a1ac1074d010d
fusilli <piotrek@bardzofajny.net>
2018-03-06 13:34:14 +0100

    added languages to about section

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
+var allChooseLangButtons = document.querySelectorAll('.choose-lang button');
+var allLanguagesTexts = document.querySelectorAll('.language');
+
+if (localStorage.getItem('language') !== null) {
+  hideAllLangsShowCurrent(localStorage.getItem('language'));
+}
+
+allChooseLangButtons.forEach(function (el) {
+  return el.addEventListener('click', handleChooseLangButtonClick);
+});
+
+function handleChooseLangButtonClick(e) {
+  hideAllLangsShowCurrent(e.target.value);
+  localStorage.setItem('language', e.target.value);
+}
+
+function hideAllLangsShowCurrent(currentLanguage) {
+  allLanguagesTexts.forEach(function (el) {
+    return el.style.display = 'none';
+  });
+  document.querySelector('.' + currentLanguage).style.display = 'block';
+}
